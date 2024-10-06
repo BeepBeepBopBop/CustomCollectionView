cc749ac3b06800039a1843d7333f21f079e19d6f
BeepBeepBopBop <evan.carrere@gmail.com>
2024-10-06 09:13:45 +0200

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StatefulContentView_Tapped(object sender, EventArgs e)
+        {
+            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Selected");
+        }
+
+        private void StatefulContentView_Hovered(object sender, EventArgs e)
+        {
+            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Hovered");
+        }
+
+        private void StatefulContentView_HoverExited(object sender, EventArgs e)
+        {
+            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Normal");
+        }
+    }
+}
