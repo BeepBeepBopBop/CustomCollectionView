733772b193999b1a7e833ef0e5c68cc0ad0be01b
BeepBeepBopBop <evan.carrere@gmail.com>
2024-10-07 07:52:34 +0200

    update

--- CustomCollectionView.xaml.cs
+++ CustomCollectionView.xaml.cs
@@ -5,18 +5,13 @@ namespace CustomCollectionView;
 
 public partial class CustomCollectionView : ContentView
 {
-    public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(nameof(ItemTemplate), typeof(DataTemplate), typeof(CustomCollectionView), propertyChanged: OnItemTemplatePropertyChanged);
+    private VisualElement? _latestSelectedVisualElement;
+
+    public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(nameof(ItemTemplate), typeof(DataTemplate), typeof(CustomCollectionView));
     public DataTemplate ItemTemplate
     {
         get => (DataTemplate)GetValue(ItemTemplateProperty);
         set => SetValue(ItemTemplateProperty, value);
-    }
-
-    public static readonly BindableProperty ItemTemplate2Property = BindableProperty.Create(nameof(ItemTemplate2), typeof(DataTemplate), typeof(CustomCollectionView));
-    public DataTemplate ItemTemplate2
-    {
-        get => (DataTemplate)GetValue(ItemTemplate2Property);
-        set => SetValue(ItemTemplate2Property, value);
     }
 
     public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable), typeof(CustomCollectionView));
@@ -59,50 +54,57 @@ public partial class CustomCollectionView : ContentView
         {
             VisualStateManager.GoToState(visualElement2, "_Selected");
         }
+
+        var child = customCollectionView.rootLayout.GetVisualTreeDescendants();
     }
 
-    private static void OnItemTemplatePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    private void OnRootLayoutChildAdded(object sender, ElementEventArgs e)
     {
-        var customCollectionView = (CustomCollectionView)bindable;
-
-        var dataTemplate = (DataTemplate)newValue;
-
-        var wrapper = new DataTemplate(() =>
-        {
-            var statefulContentView = new StatefulContentView();
-            statefulContentView.Tapped += StatefulContentView_Tapped;
-            return statefulContentView;
-        });
-    }
-
-    private static void StatefulContentView_Tapped(object? sender, EventArgs e)
-    {
-    }
-
-    private void rootLayout_ChildAdded(object sender, ElementEventArgs e)
-    {
-        VerticalStackLayout verticalStackLayout;
-
         if (e.Element is View view)
         {
             TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer()
             {
                 NumberOfTapsRequired = 1,
             };
-            PointerGestureRecognizer pointerGestureRecognizer = new PointerGestureRecognizer()
-            {
-            };
 
-            pointerGestureRecognizer.PointerEntered += PointerGestureRecognize_PointerEntered;
-            pointerGestureRecognizer.PointerExited += PointerGestureRecognize_PointerExited;
-            tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+            PointerGestureRecognizer pointerGestureRecognizer = new PointerGestureRecognizer();
+
+            pointerGestureRecognizer.PointerEntered += OnPointerGestureRecognizePointerEntered;
+            pointerGestureRecognizer.PointerExited += OnPointerGestureRecognizePointerExited;
+            tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
 
             view.GestureRecognizers.Add(tapGestureRecognizer);
             view.GestureRecognizers.Add(pointerGestureRecognizer);
         }
     }
 
-    private void PointerGestureRecognize_PointerExited(object? sender, PointerEventArgs e)
+    private void rootLayout_Loaded(object sender, EventArgs e)
+    {
+        if (SelectedItem != null)
+        {
+            foreach (var child in rootLayout.Children)
+            {
+                if (child is VisualElement visualElement && visualElement.BindingContext == SelectedItem)
+                {
+                    SetSelectedElement(visualElement);
+                }
+            }
+        }
+    }
+
+    private void SetSelectedElement(VisualElement visualElement)
+    {
+        if (_latestSelectedVisualElement != null)
+        {
+            VisualStateManager.GoToState(_latestSelectedVisualElement, "_Normal");
+        }
+
+        _latestSelectedVisualElement = visualElement;
+        VisualStateManager.GoToState(visualElement, "_Selected");
+        SelectedItem = visualElement.BindingContext;
+    }
+
+    private void OnPointerGestureRecognizePointerExited(object? sender, PointerEventArgs e)
     {
         if (sender is VisualElement visualElement)
         {
@@ -112,7 +114,7 @@ public partial class CustomCollectionView : ContentView
         }
     }
 
-    private void PointerGestureRecognize_PointerEntered(object? sender, PointerEventArgs e)
+    private void OnPointerGestureRecognizePointerEntered(object? sender, PointerEventArgs e)
     {
         bool isSelected = ((VisualElement)sender).BindingContext == SelectedItem;
 
@@ -122,17 +124,11 @@ public partial class CustomCollectionView : ContentView
         }
     }
 
-    private void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
+    private void OnTapGestureRecognizerTapped(object? sender, TappedEventArgs e)
     {
         if (sender is VisualElement visualElement)
         {
-            VisualStateManager.GoToState(visualElement, "_Selected");
-            SelectedItem = visualElement.BindingContext;
+            SetSelectedElement(visualElement);
         }
-    }
-
-    protected override void OnChildAdded(Element child)
-    {
-        base.OnChildAdded(child);
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,20 +15,5 @@
 
             BindingContext = mainViewModel;
         }
-
-        private void StatefulContentView_Tapped(object sender, EventArgs e)
-        {
-            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Selected");
-        }
-
-        private void StatefulContentView_Hovered(object sender, EventArgs e)
-        {
-            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Hovered");
-        }
-
-        private void StatefulContentView_HoverExited(object sender, EventArgs e)
-        {
-            Microsoft.Maui.Controls.VisualStateManager.GoToState((VisualElement)sender, "Normal");
-        }
     }
 }
